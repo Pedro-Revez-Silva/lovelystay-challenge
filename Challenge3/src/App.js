590cfac2a3b01379740f204777bfcd6ec0e5aa29
Pedro Silva <pedro.revez.silva@gmail.com>
2018-12-05 12:02:06 +0000

    Correctly Display information for single user

--- Challenge3/src/App.js
+++ Challenge3/src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import UserInformation from './UserInformation';
-import ReactDOM from 'react-dom'
+
 
 class App extends Component {
 
@@ -15,13 +14,13 @@ class App extends Component {
   }
 
   getUserInformation(username) {
-    return fetch('https://api.github.com/users/${username}')
+    return fetch('https://api.github.com/users/Pedro-Revez-Silva')
     .then(response => response.json())
     .then(response => {return response})
   }
   
   getUserRepos(username){
-    return fetch('https://api.github.com/users/${username}/repos')
+    return fetch('https://api.github.com/users/Pedro-Revez-Silva/repos')
     .then(response => response.json())
     .then(response => {return response})
   }
@@ -29,9 +28,11 @@ class App extends Component {
   /* This method will fetch the user and repo information and update the state*/
   async submitRequest(e){
     e.preventDefault();
+    var button = document.getElementById("button");
+    button.style.visibility = 'hidden';
 
     const {value} = this.refs.username;
-    
+
     let user = await this.getUserInformation(value);
     let repos = await this.getUserRepos(value);
 
@@ -43,14 +44,7 @@ class App extends Component {
         following: user.following,
         url: user.url,
       },
-      repos: {name: repos.name,
-        description: repos.description,
-        git_url: repos.git_url,
-        stargazers_count: repos.stargazers_count,
-        forks_count: repos.forks_count,
-        open_issues_count: repos.open_issues_count,
-        size: repos.size,
-      }
+      repos,
     }); 
   }
 
@@ -60,6 +54,7 @@ class App extends Component {
       return <div key={item.id} className="repoResults">
         <p>
           {item.name}
+          {item.desccription}
         </p>
       </div>
     })
@@ -98,10 +93,7 @@ class App extends Component {
         <div className="App-intro">
           <hr />
           <p>Click on the button to fetch the user information</p>
-          <form onSubmit={e => this.submitRequest(e)}>
-            <input ref='username' type='text' placeholder='username' />
-          </form>
-          <button ref='username' defaultValue='Pedro-Revez-Silva' onClick={e => this.submitRequest(e)}>
+          <button id ='button' ref='username' defaultValue='Pedro-Revez-Silva' onClick={e => this.submitRequest(e)}>
             Click me
           </button>
           <div className="Search-intro">
