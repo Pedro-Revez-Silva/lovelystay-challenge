c58138bdd6e970ce7645b7115e4c4663c2d72360
Pedro Silva <pedro.revez.silva@gmail.com>
2018-12-07 10:11:35 +0000

    Implement tests - Element render

--- Challenge3/src/App.test.js
+++ Challenge3/src/App.test.js
@@ -7,12 +7,25 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
 });
 
-it('User information request is successful', ()=>{
-  const request = getUserInformation('Pedro-Revez-Silva');
-  
+it('always renders a div', () => {
+  const divs = render(<App/>).find('div');
+  expect(divs.length).toBeGreaterThan(0);
 });
 
-it('Repo information request is successful', ()=>{
-  const request = getUserRepos('Pedro-Revez-Silva');
-  
+describe('the rendered div', () => {
+  it('contains everything else that gets rendered', () => {
+    const divs = render(<App/>).find('div');
+    const wrappingDiv = divs.first();
+
+    expect(wrappingDiv.children()).toEqual(App().children());
+  });
 });
+
+it('Always renders a button', () => {
+  expect(App().find('button').length).toBeGreaterThan(1);
+})
+
+/* TO-DO
+Implement tests with a mock API
+Implement tests for the onclick event
+Implement tests for the render of user and repo information */
